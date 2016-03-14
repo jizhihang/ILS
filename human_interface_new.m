d72ae3edba823ea7d36ff2235ff91332fd53b47d
Addison Bohannon <addison.bohannon@gmail.com>
2016-03-14 16:54:21 -0400

    Working on the human interface

---
+++ human_interface_new.m
@@ -0,0 +1,35 @@
+function gui = human_interface_new(human,images)
+% EXPERIMENT_INTERFACE
+
+    gui = figure('Visible','off','Position',[300,300,450,550]);
+    image = axes('Units','pixels','Position',[50,150,350,350]);
+    targetButton = uicontrol('Style','pushbutton','String','Target',...
+        'Position',[100,50,100,50],'Callback',@targetButton_callback);
+    nonTargetButton = uicontrol('Style','pushbutton','String','Non-target',...
+        'Position',[250,50,100,50],'Callback',@nonTargetButton_callback);
+    
+    image.Units = 'normalized';
+    targetButton.Units = 'normalized';
+    nonTargetButton.Units = 'normalized';
+    
+    n = length(images);
+    k = 1;
+    imshow(images(k).name);
+    
+    gui.Name = 'Human';
+    movegui(gui,'center')
+    gui.Visible = 'on';
+    
+    function targetButton_callback(source,eventdata) 
+    % TARGETBUTTON_CALLBACK classifies the image as a target
+        if k < n
+            human.response(k) = 1;
+            k = k + 1;
+            imshow(images(k).name);
+        else
+            notify(human,'iterationComplete');
+        end
+    end
+
+end
+
