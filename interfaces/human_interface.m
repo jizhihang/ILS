075a54dab5387319651dc2a1b23c12f26f19fc9d
nwayt001 <nick.waytowich@gmail.com>
2016-03-16 18:31:08 -0400

    Fixed bug in human interface
    Fixed a bug in the human interface where the nested functions and use of persistence variables was somehow causing a segmentation fault in matlab.

--- interfaces/human_interface.m
+++ interfaces/human_interface.m
@@ -2,13 +2,14 @@ function gui = human_interface(human,images)
 % HUMAN_INTERFACE provides the binary classification interface for the
 % human self-paced labeler. It loads the assigned images and writes the
 % classification responses to a field in the human object.
-
-    gui = figure('Visible','off','Position',[300,300,450,550]);
-    image = axes('Units','pixels','Position',[50,150,350,350]);
-    targetButton = uicontrol('Style','pushbutton','String','Target',...
-        'Position',[100,50,100,50],'Callback',@targetButton_callback);
-    nonTargetButton = uicontrol('Style','pushbutton','String','Non-target',...
-        'Position',[250,50,100,50],'Callback',@nonTargetButton_callback);
+global k n  gImages gHuman;
+    gui = figure('Visible','off','Position',[300,300,750,750],'DockControls','off'...
+        ,'MenuBar','none','NumberTitle','off');
+    image = axes('Units','pixels','Position',[50,150,650,550]);
+    targetButton = uicontrol('Style','pushbutton','String','Target','FontSize',14,...
+        'Position',[225,50,125,65],'Callback',@targetButton_callback);
+    nonTargetButton = uicontrol('Style','pushbutton','String','Non-target','FontSize',14,...
+        'Position',[400,50,125,65],'Callback',@nonTargetButton_callback);
     
     image.Units = 'normalized';
     targetButton.Units = 'normalized';
@@ -18,34 +19,47 @@ function gui = human_interface(human,images)
     k = 1;
     imshow(images{k});
     
-    gui.Name = 'Human';
+    gui.Name = 'Human Agent';
     movegui(gui,'center')
     gui.Visible = 'on';
     
-    function targetButton_callback(source,eventdata) 
-    % TARGETBUTTON_CALLBACK classifies the image as a target
-        if k <= n
-            human.response(k) = 1;
-            if k < n
-                k = k + 1;
-                imshow(images{k});
-                return;
-            end
-            notify(human,'iterationComplete');
-        end
-    end
+gImages = images;
+gHuman = human;
 
-    function nonTargetButton_callback(source,eventdata) 
-    % NONTARGETBUTTON_CALLBACK classifies the image as a target
-        if k <= n
-            human.response(k) = 1;
-            if k < n
-                k = k + 1;
-                imshow(images{k});
-                return;
-            end
-            notify(human,'iterationComplete');
-        end
-    end
+end
 
+function targetButton_callback(~,~)
+% TARGETBUTTON_CALLBACK - callback function for the human interface
+% selection of the target button. this function records the human
+% classification, updates the image and notifies the listener when all
+% images have been classified.
+global gImages k n gHuman
+% TARGETBUTTON_CALLBACK classifies the image as a target
+if k <= n
+    gHuman.response(k) = 1;
+    if k < n
+        k = k + 1;
+        imshow(gImages{k});
+        return;
+    end
+    notify(gHuman,'iterationComplete');
+end
+end
+
+function nonTargetButton_callback(~,~)
+% NONTARGETBUTTON_CALLBACK - callback function for the human interface
+% selection of the non-target button. this function records the human
+% classification, updates the image and notifies the listener when all
+% images have been classified.
+% NONTARGETBUTTON_CALLBACK classifies the image as a target
+global gImages k n gHuman
+if k <= n
+    gHuman.response(k) = 1;
+    if k < n
+        k = k + 1;
+        imshow(gImages{k});
+        return;
+    end
+    notify(gHuman,'iterationComplete');
+end
 end
