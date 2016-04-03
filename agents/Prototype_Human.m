9c84dc311b1571551d8c4633c10e7f565e434f71
addisonbohannon <addison.bohannon@gmail.com>
2016-04-03 11:53:32 -0400

    Making changes to prototype agents

--- agents/Prototype_Human.m
+++ agents/Prototype_Human.m
@@ -6,6 +6,7 @@ classdef Prototype_Human < RemoteAgent
     properties
         accuracy % Accuracy of prototype agent
         delay % Delay of prototype agent
+        trueLabels % True labels of experiment
         trueBehavior % Delay is manifest in agent (boolean)
     end
     
@@ -13,21 +14,30 @@ classdef Prototype_Human < RemoteAgent
         % -----------------------------------------------------------------
         % Class constructor:
         
-        function A = Prototype_Human(remotePort,agentAccuracy,trueBehavior)
+        function A = Prototype_Human(remotePort,varargin)
+        % PROTOTYPE_HUMAN is the class constructor of the prototype human
+        % child class of remote agent. It takes variable name-value pair
+        % options: H = Prototype_Human(9999, 'accuracy', 0.99,
+        % 'trueBehavior', true, 'trueLabels', labels);
             if nargin < 1
                 error('Too few parameters for class construction.');
             end
             A@RemoteAgent('prototype_human',remotePort);
             A.delay = 1;
-            if nargin >= 2
-                A.accuracy = agentAccuracy;
-            else
-                A.accuracy = rand;
-            end
-            if nargin == 3
-                A.trueBehavior = trueBehavior;
-            else
-                A.trueBehavior = false;
+            A.accuracy = rand;
+            A.trueBehavior = false;
+            A.trueLabels = [];
+            if nargin > 1
+                for i = 1:2:length(varargin)
+                    switch varargin{i}
+                        case 'accuracy'
+                            A.accuracy = varargin{i+1};
+                        case 'trueBehavior'
+                            A.trueBehavior = varargin{i+1};
+                        case 'trueLabels'
+                            A.trueLabels = varargin{i+1}';
+                    end
+                end
             end
         end
         
@@ -42,19 +52,25 @@ classdef Prototype_Human < RemoteAgent
             X = fread(obj.socket);
             if strcmp(char(X)','complete')
                 terminate(obj);
+                return
             elseif strcmp(char(X)','test')
                 return
-            else
+            elseif isempty(obj.trueLabels)
                 n = length(X);
                 Y = zeros(n,1);
                 Y(rand(n,1)<obj.accuracy) = 1;
-                if obj.trueBehavior
-                    pause(n*obj.delay);
-                end
-                fwrite(obj.socket,Y(:));
-                fprintf('Prototype_Human completed classification of %u images.\n',...
-                    n)
+            else
+                n = length(X);
+                Y = obj.trueLabels(X);
+                index = rand(n,1)>obj.accuracy;
+                Y(index) = -Y(index);
             end
+            if obj.trueBehavior
+                pause(n*obj.delay);
+            end
+            fwrite(obj.socket,Y(:));
+            fprintf('Prototype_Human completed classification of %u images.\n',...
+                n)
         end
         
         %------------------------------------------------------------------
